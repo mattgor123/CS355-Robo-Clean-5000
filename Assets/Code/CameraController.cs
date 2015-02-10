6724d36f88940346f29e9486f9057c286279aace
George Jiang <gjiang2@jhu.edu>
2015-02-09 22:42:50 -0500

    Added code for player and camera. Camera follows player with "forward" direction upward. Updated scene.

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	[SerializeField]
+	private PlayerController player;
+
+    [SerializeField]
+    private float turnrate;
+
+    [SerializeField]
+    private float n_offset;
+
+	private Vector3 offset;
+    private float height;
+    private float angle; //in degrees
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+        height = transform.position.y;
+        angle = 0;
+	}
+
+    void Update()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        angle = transform.eulerAngles.y + turn*turnrate;
+        if (angle > 360){
+            angle -= 360;
+        }
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, angle, 0);  //Rotate to right facing
+
+        float z = Mathf.Cos(angle * Mathf.PI/180)*n_offset;
+        float x = Mathf.Sin(angle * Mathf.PI/180) * n_offset;
+        offset = new Vector3(x, height, z);
+    }
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+
+    public float getAngle()
+    {
+        return angle;
+    }
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	private float speed;
+
+    [SerializeField]
+    private CameraController cam;
+
+	void Start() {
+	}
+
+	//Physics Code
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");    //forward movement; turning linked to camera
+
+        float angle = cam.getAngle() * Mathf.PI/180;
+
+        float z = Mathf.Cos(angle) * moveVertical;
+        float x = Mathf.Sin(angle) * moveVertical;
+  
+		Vector3 mvt = new Vector3(x, 0.0f, z);
+		rigidbody.AddForce (mvt * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+	}
+	
+}
