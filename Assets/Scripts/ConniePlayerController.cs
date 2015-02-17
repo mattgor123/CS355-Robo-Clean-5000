166626a1cbbca7fcb485d2f45d0141e6fb6780e1
George Jiang <gjiang2@jhu.edu>
2015-02-16 20:45:00 -0500

    Changed class name to avoid conflict.

--- Assets/Scripts/ConniePlayerController.cs
+++ Assets/Scripts/ConniePlayerController.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class CPlayerController : MonoBehaviour {
 
 	public float speed;
 	public Text scoreText;
