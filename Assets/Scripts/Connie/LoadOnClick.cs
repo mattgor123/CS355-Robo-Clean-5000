88aab2dae023de69ced93767432386459cba2e0f
Connie Chang <conniechang888@gmail.com>
2015-02-24 19:25:04 -0500

    Generic main menu added. Play/New Game leads to Main scene

---
+++ Assets/Scripts/Connie/LoadOnClick.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject menuScreen;
+
+    [SerializeField]
+    private GameObject optionsScreen;
+
+    [SerializeField]
+    private GameObject playMenuScreen;
+
+    void Start()
+    {
+        optionsScreen.SetActive(false);
+        menuScreen.SetActive(true);
+        playMenuScreen.SetActive(false);
+    }
+
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void OpenOptions()
+    {
+        menuScreen.SetActive(false);
+        optionsScreen.SetActive(true);
+        playMenuScreen.SetActive(false);
+    }
+
+    public void OpenPlayMenu()
+    {
+        menuScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        playMenuScreen.SetActive(true);
+    }
+
+    public void OpenMainMenu()
+    {
+        menuScreen.SetActive(true);
+        optionsScreen.SetActive(false);
+        playMenuScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Connie/NewRoomTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEnterRoom : MonoBehaviour {
+    [SerializeField]
+    int level;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(level);
+        }
+    }
+}
