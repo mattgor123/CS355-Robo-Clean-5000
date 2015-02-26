de99f4b311ada96df8992c22be440130a8b12239
George Jiang <gjiang2@jhu.edu>
2015-02-26 16:52:59 -0500

    Changed lights to stay dimly lit instead of going off when untriggered after initial activation.

--- Assets/Scripts/GeorgeMazeScene/WallLightController.cs
+++ Assets/Scripts/GeorgeMazeScene/WallLightController.cs
@@ -4,13 +4,19 @@ using System.Collections;
 public class WallLightController : MonoBehaviour {
     [SerializeField]
     private TriggerTile TTile;
-    [SerializeField]
-    private float strength;
 
+    [SerializeField]
+    private float strength; //intensity of light when fully lit
+
+    [SerializeField]
+    private float dim;      //intensity of light when dimmed (after initial activation)
+
+    private bool activated; //whether light has been activated
 
 	// Use this for initialization
 	void Start () {
-	
+	    light.intensity = 0;
+        activated = false;
 	}
 	
     // Link a trigger tile
@@ -24,10 +30,13 @@ public class WallLightController : MonoBehaviour {
         if (TTile.GetTriggerStatus())
         {
             light.intensity = strength;
+            activated = true;
+        }
+        else if (activated)
+        {
+            light.intensity = dim;
         }
         else
-        {
-            light.intensity = 0;
-        }
+            light.intensity = 0;       
 	}
 }
