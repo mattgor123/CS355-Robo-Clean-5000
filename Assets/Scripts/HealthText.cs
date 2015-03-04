09f3292badb483cda01bf9cef7ad20042a1bd6ca
Greg - Main <gregadoff@GA-MBP15.local>
2015-03-03 20:03:20 -0500

    hud

---
+++ Assets/Scripts/HealthText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthText : MonoBehaviour {
+	
+	//[SerializeField]
+	private HealthController health_controller;
+	
+	//private GUIText weapon_name;
+	private Text player_health;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.FindWithTag("Player");
+		health_controller = player.GetComponent<HealthController>();
+		player_health = GetComponent<Text>();
+		
+	}
+	
+	private void LateUpdate()
+	{
+			//Debug.Log(weapon_backpack_controller.GetWeaponName());
+			player_health.text = "Health: " + health_controller.GetCurrentHealth ().ToString ();
+	}
+}
