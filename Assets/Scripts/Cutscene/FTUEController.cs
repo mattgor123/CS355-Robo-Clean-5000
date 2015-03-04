2787140aa161c6b3d63d6067c1ed1138662e862d
Matt Gorelik <matthewgorelik@gmail.com>
2015-03-03 21:18:01 -0500

    Adding FTUE Scene

---
+++ Assets/Scripts/Cutscene/FTUEController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FTUEController : MonoBehaviour {
+
+    public string nextScene;
+
+    public void OnGUI() { if (Event.current.type == EventType.keyDown) {
+        ChangeLevel();
+    }}
+
+    private void ChangeLevel()
+    {
+        Application.LoadLevel(nextScene);
+    }
+}
