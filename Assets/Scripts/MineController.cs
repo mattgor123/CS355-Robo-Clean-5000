8065ba99ee7f4175c2f63809a89ff3a426bdd92d
Anthony Burzillo <burz@jhu.edu>
2015-03-11 15:55:30 -0400

    Add mine controller (not linked up yet)

---
+++ Assets/Scripts/MineController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineController : MonoBehaviour {
+
+    [SerializeField] private GameObject explosion;
+
+    private void OnCollisionEnter (Collision collision) {
+        var explosion_instance = Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+        Destroy(gameObject);
+    }
+}
