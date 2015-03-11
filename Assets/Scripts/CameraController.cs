98ad278af22f5d86273543aa373bfc57d83bd16d
George Jiang <gjiang2@jhu.edu>
2015-03-11 15:04:39 -0400

    Fixed lighting issue.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,7 +10,7 @@ public class CameraController : MonoBehaviour {
 
 	void Start () {
         Player = GameObject.FindGameObjectWithTag("Player");
-		offset = transform.position;
+        offset = new Vector3(0f, 10f, -5f);//transform.position;
         TrackFace = false;
 	}
 
