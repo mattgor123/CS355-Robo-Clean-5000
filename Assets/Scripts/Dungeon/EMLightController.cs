9bb9a5c4c1a178d7bf85f29284c9178c33fb34a5
George Jiang <gjiang2@jhu.edu>
2015-03-30 22:46:11 -0400

    Added reduced angle wall lights,  180EML and 90EML.  Use 180 for lengths of wall and 90 for corners.
    The positions/rotations should be aligned such that setting the prefab's x & z position and y rotation equal to that of the wall will align the light with that wall.
    
    For corners, position is at the corner itself. Rotation should be the same as the right side wall when facing the corner.

---
+++ Assets/Scripts/Dungeon/EMLightController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Generic Emergency Light controller
+// Activates upon trigger & stays on if linked to a trigger tile
+// Else stays on
+// Note: position is 4.5 from a wall, in the direction of its face
+public class EMLightController : MonoBehaviour {
+
+    [SerializeField]
+    private TriggerTile TTile;
+
+    [SerializeField]
+    private float strength; //intensity of light when fully lit
+
+    [SerializeField]
+    private float dim;      //intensity of light when not directly triggered
+
+    private bool activated; //whether light has been activated
+    private Light[] EML;      //the light object children
+    private int numLights;      //number of lights
+
+    // Use this for initialization
+    void Start()
+    {
+        //Attach subcomponent light
+        EML = GetComponentsInChildren<Light>();
+        numLights = EML.Length;
+
+    }
+
+    // Link a trigger tile
+    public void LinkTile(Transform tile)
+    {
+        TTile = tile.GetComponent<TriggerTile>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //If no tile attached, or active but not triggered, keep dim
+        if (TTile == null || activated) 
+        {
+            SetIntensity(dim);
+        }
+
+        //On trigger, flag activated and keep at full strength       
+        else if (TTile.GetTriggerStatus())
+        {
+            SetIntensity(strength);
+            activated = true;
+        }
+        else
+            SetIntensity(0f);
+
+
+    }
+
+    //Set intensity of all lights
+    private void SetIntensity(float intense)
+    {
+        for (int i = 0; i < numLights; i++)
+        {
+            EML[i].intensity = intense;
+        }
+    }
+}
+
