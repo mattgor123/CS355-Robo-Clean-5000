014e36c12dd69b0a6654fc81fcd5e02182033868
Greg - Main <gregadoff@GA-MBP15.local>
2015-04-07 17:57:23 -0400

    bar flashes when hit

--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -9,7 +9,11 @@ public class HealthBarController : MonoBehaviour {
 	
 	//private GUIText weapon_name;
 	private Slider slider;
-	
+
+	private float flashFor; //flash for how many seconds after being hit?
+	private float flashUntil; //stop flashing when this time is reached
+	private float mostRecentHealth; //use to check if health has dropped since last frame
+
 	// Use this for initialization
 	void Start () {
 		GameObject player = GameObject.FindWithTag("Player");
@@ -17,12 +21,19 @@ public class HealthBarController : MonoBehaviour {
 		slider = GameObject.FindWithTag ("HealthSlider").GetComponent<Slider>();
 		slider.minValue = 0;
 		slider.maxValue = 0;
+		flashFor = 2.0f;
+		flashUntil = 0;
+		mostRecentHealth = 0;
 	}
 	
 	private void LateUpdate()
 	{
 		//Debug.Log(weapon_backpack_controller.GetWeaponName());
 		if (health_controller != null) {
+			if (health_controller.GetCurrentHealth() < mostRecentHealth) {
+				flashUntil = Time.time + flashFor;
+			}
+			mostRecentHealth = health_controller.GetCurrentHealth();
 			slider.maxValue = health_controller.GetMaxHealth();
 			slider.value = health_controller.GetCurrentHealth();
 			//update color here, using RGBA values
@@ -58,6 +69,16 @@ public class HealthBarController : MonoBehaviour {
 		return 0;
 	}
 	private float getA() {
-		return 1;
+		if (flashUntil < Time.time) {
+			return 0.75f;
+		} else {
+			float remaining = flashUntil - Time.time;
+			float fr = remaining / flashFor;
+			if ((fr < .1) || (fr > .2 && fr < .3) || (fr > .4 && fr <.5) || (fr > .6 && fr < .7) || (fr > .8 && fr < .9)) {
+				return 1;
+			} else {
+				return 0.5f;
+			}
+		}
 	}
 }
