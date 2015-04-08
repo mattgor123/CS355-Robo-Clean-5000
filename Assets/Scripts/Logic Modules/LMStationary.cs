9903a6071141fe8245c32142eedcc14f89ec3ae5
George Jiang <gjiang2@jhu.edu>
2015-04-08 16:26:56 -0400

    Turret fix.

---
+++ Assets/Scripts/Logic Modules/LMStationary.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Logic Module: Hold Range 
+// tries to stay within some optimal range band with respect to target
+public class LMStationary : MonoBehaviour, LMMove {
+
+    public Vector3 MoveLogic(EnemyController enemy, GameObject player)
+    {
+        float dist = LMHelper.GetDistance(enemy, player);
+        Vector3 mvt = new Vector3(0f, 0f, 0f);
+        enemy.SetStationary(true);
+
+        // If out of range, move into range
+        if (dist <= enemy.GetAggroRadius())
+        {
+            enemy.SetAggroState(true);
+        }
+        else {
+            enemy.SetAggroState(false);
+        }
+        return mvt;
+    }
+}
