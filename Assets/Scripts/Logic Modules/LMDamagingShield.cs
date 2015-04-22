7d2c4f56c03ce55246578a7d321e52414d2c9451
George Jiang <gjiang2@jhu.edu>
2015-04-21 22:32:04 -0400

    Added damaging shield module. Deals damage to player on collision and collision stay

---
+++ Assets/Scripts/Logic Modules/LMDamagingShield.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Logic Module: Damaging Shield
+// deals damage to colliding foes when the shield is active
+public class LMDamagingShield : MonoBehaviour {
+
+    [SerializeField]
+    private float Damage = -50f;
+
+    private ShieldController Shield;
+
+	// Use this for initialization
+	void Start () {
+        Shield = GetComponentInChildren<ShieldController>();
+	}
+
+    //Deal some damage on initial collision
+    void OnCollisionEnter(Collider other)
+    {
+        GameObject OGO = other.gameObject;
+        if (OGO.tag == "Player")
+        {
+            HealthController HC = OGO.GetComponent<HealthController>();
+            HC.ChangeHealth(Damage);
+        }
+    }
+
+    //Deal some sustained damage on continued collision
+    void OnCollisionStay(Collider other)
+    {
+        GameObject OGO = other.gameObject;
+        if (OGO.tag == "Player")
+        {
+            HealthController HC = OGO.GetComponent<HealthController>();
+            HC.ChangeHealth(Damage * 0.1f);
+        }
+        
+    }
+}
