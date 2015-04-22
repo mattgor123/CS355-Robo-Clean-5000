40be2da8dc79bf182c360bc0bb46d2fc03cc57ad
Greg - Main <gregadoff@GA-MBP15.local>
2015-04-22 12:57:13 -0400

    canvas for statistics, pops up with elevator canvas

---
+++ Assets/Scripts/StatisticsCanvasController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatisticsCanvasController : MonoBehaviour {
+
+	private GameObject statCanvas;
+	private GameObject elevCanvas;
+	// Use this for initialization
+	void Start () {
+		elevCanvas = GameObject.FindGameObjectWithTag ("ElevatorCanvas");
+		statCanvas = GameObject.FindGameObjectWithTag ("StatCanvas");
+		statCanvas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (statCanvas == null) {
+			Debug.Log ("stat null");
+			statCanvas = GameObject.FindGameObjectWithTag ("StatCanvas");
+		}
+		if (elevCanvas == null) {
+			Debug.Log ("elev null");
+			elevCanvas = GameObject.FindGameObjectWithTag ("ElevatorCanvas");
+		}
+		if (statCanvas != null && elevCanvas != null) {
+			statCanvas.SetActive (elevCanvas.activeSelf);
+		}
+	}
+}
---
+++ Assets/Scripts/StatisticsRecorderController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatisticsRecorderController : MonoBehaviour {
+
+	[SerializeField] private GameObject statCanvas;
+
+	// Use this for initialization
+	void Start () {
+		statCanvas = GameObject.FindGameObjectWithTag ("StatCanvas");
+		statCanvas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
