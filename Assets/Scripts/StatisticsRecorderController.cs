a6b6703c0cc736a606cddf672f421877f3d687a9
Greg - Main <gregadoff@GA-MBP15.local>
2015-04-22 13:25:48 -0400

    stat canvas tweaks, time elapsed statistic added

--- Assets/Scripts/StatisticsRecorderController.cs
+++ Assets/Scripts/StatisticsRecorderController.cs
@@ -1,18 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StatisticsRecorderController : MonoBehaviour {
 
-	[SerializeField] private GameObject statCanvas;
+	private Text timeText;
+	private float timeAtStart;
+	private float timeNow;
+	private float timeElapsed;
 
 	// Use this for initialization
 	void Start () {
-		statCanvas = GameObject.FindGameObjectWithTag ("StatCanvas");
-		statCanvas.SetActive (false);
+		timeAtStart = Time.time;
+		timeNow = Time.time;
+		timeElapsed = timeAtStart - timeNow;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (timeText == null) {
+			timeText = GameObject.FindGameObjectWithTag ("TimeText").GetComponent<Text> ();
+		}
+		if (timeText != null) {
+			timeNow = Time.time;
+			timeElapsed = timeNow - timeAtStart;
+			timeText.text = "Time Elapsed: " + timeElapsed.ToString();
+		}
 	}
 }
