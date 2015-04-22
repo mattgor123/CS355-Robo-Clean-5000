bb325b7120691b7c0fb8c2f123966ee43a021ca3
Matt Gorelik <mgoreli5@jhu.edu>
2015-04-22 00:42:15 -0400

    Implemented fire damage - smoky fire does more damage than straight fire. don't stand in fire! you might die. fire is hot.

---
+++ Assets/Scripts/Effects/FireCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireCollider : MonoBehaviour {
+
+    [SerializeField]
+    private float Damage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Deal damage on initial contact; hits enemies too
+        GameObject OGO = other.gameObject;
+        if (OGO.tag == "Player")
+        {
+            HealthController HC = OGO.GetComponent<HealthController>();
+            HC.ChangeHealth(Damage);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        {
+            GameObject OGO = other.gameObject;
+            if (OGO.tag == "Player")
+            {
+                HealthController HC = OGO.GetComponent<HealthController>();
+                HC.ChangeHealth(Damage * 0.1f);
+            }
+        }
+    }
+
+}
