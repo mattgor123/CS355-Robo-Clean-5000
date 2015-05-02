857466bebb1bba4e3ea39e95c4ce00af28a04df9
Craig Hennessy <chennes3@gmail.com>
2015-05-02 04:08:37 -0400

    Improved Elevator in uGUI. Still WIP found in Test_UI_Room.unity. Needs to be transferred into game before button connections can be made (needs to be able to touch StageBuilder and Player)

---
+++ Assets/ButtonListController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonListController : MonoBehaviour {
+
+    [SerializeField]
+    private Button[] buttons;
+    [SerializeField]
+    private bool[] testKeys;
+    [SerializeField]
+    private int testFloor;
+
+    private InventoryController inv;
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+	    
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i == testFloor) //Can't go to current floor
+            {
+                buttons[i].interactable = false;
+            }
+            else if (!testKeys[i]) //Can't go to floor without having the key
+            {
+                buttons[i].interactable = false;
+            }
+            else
+            {
+                buttons[i].interactable = true; //Else the button is pressable
+            }
+        }
+	
+	}
+}
